e7b1904fec97d697bc5b3b77668f04019cf155fe
Anthony Fu <anthonyfu117@hotmail.com>
2023-04-24 04:03:09 +0200

    chore: release v0.1.3

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Clean Twitter
 // @namespace    http://antfu.me/
-// @version      0.0.0
+// @version      0.1.3
 // @description  Bring back peace on Twitter
 // @author       Anthony Fu (https://github.com/antfu)
 // @license      MIT

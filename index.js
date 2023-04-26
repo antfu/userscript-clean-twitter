3b05988d8f8ac791078d26e7744530f73edec3e9
冯不游 <71683364+mefengl@users.noreply.github.com>
2023-04-26 13:31:25 +0800

    chore: add filter toggle menu

--- index.js
+++ index.js
@@ -9,12 +9,58 @@
 // @supportURL   https://github.com/antfu/userscript-clean-twitter
 // @match        https://twitter.com/**
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=twitter.com
-// @grant        none
+// @grant        GM_getValue
+// @grant        GM_setValue
+// @grant        GM_registerMenuCommand
+// @grant        GM_unregisterMenuCommand
 // @run-at       document-start
 // ==/UserScript==
 
 (function () {
   'use strict'
+
+  const defaultMenuAll = {
+    'Hide Home tabs': true,
+  }
+  const menuAll = GM_getValue('menu_all', defaultMenuAll)
+  function syncMenuWithDefault(menuAll, defaultMenuAll) {
+  // Add new menu from default, if not exist yet
+    for (const name in defaultMenuAll) {
+      if (!(name in menuAll))
+        menuAll[name] = defaultMenuAll[name]
+    }
+    // Remove old menu, if not exist in default
+    for (const name in menuAll) {
+      if (!(name in defaultMenuAll))
+        delete menuAll[name]
+    }
+    GM_setValue('menu_all', menuAll)
+  }
+  syncMenuWithDefault(menuAll, defaultMenuAll)
+
+  const menuIds = GM_getValue('menu_ids', {})
+  function registerMenuCommand(name, value) {
+    const menuText = ` ${name}: ${value ? '✅' : '❌'}`
+    const commandCallback = () => {
+      menuAll[name] = !menuAll[name]
+      GM_setValue('menu_all', menuAll)
+      updateMenu()
+      location.reload()
+    }
+    return GM_registerMenuCommand(menuText, commandCallback)
+  }
+  function updateMenu() {
+    for (const name in menuAll) {
+      const value = menuAll[name]
+      if (menuIds[name])
+        GM_unregisterMenuCommand(menuIds[name])
+
+      menuIds[name] = registerMenuCommand(name, value)
+    }
+    GM_setValue('menu_ids', menuIds)
+  }
+  updateMenu()
+
   const style = document.createElement('style')
   const hides = [
     // verified badge
@@ -28,10 +74,10 @@
     // "Verified" tab
     '[role="presentation"]:has(> [href="/notifications/verified"][role="tab"])',
     // Home tabs
-    '[role="tablist"]:has([href="/home"][role="tab"])',
+    menuAll['Hide Home tabs'] ? '[role="tablist"]:has([href="/home"][role="tab"])' : '',
   ]
   style.innerHTML = [
-    `${hides.join(',')}{ display: none !important; }`,
+    `${hides.filter(s => s !== '').join(',')}{ display: none !important; }`,
     // styling
     '[aria-label="Search Twitter"] { margin-top: 20px !important; }',
   ].join('')

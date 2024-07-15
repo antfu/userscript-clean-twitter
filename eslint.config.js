d76cef5393b0b8cefb9b6216e6d87945ee46d82e
Anthony Fu <github@antfu.me>
2024-07-15 14:39:53 +0200

    chore: update deps

---
+++ eslint.config.js
@@ -0,0 +1,11 @@
+import antfu from '@antfu/eslint-config'
+
+export default antfu({
+  languageOptions: {
+    globals: {
+      GM_getValue: 'readonly',
+      GM_setValue: 'readonly',
+      GM_registerMenuCommand: 'readonly',
+    },
+  },
+})
